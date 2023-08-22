b93a704cc8c62d3dd11eca9c4ef8936a06eed8ee
aaingyunii <31847834+aaingyunii@users.noreply.github.com>
2023-08-23 00:34:33 +0900

    spring boot api testing

---
+++ springboot_tests/api/src/main/java/com/api/api/controller/DeleteController.java
@@ -0,0 +1,16 @@
+package com.api.api.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/del-api")
+public class DeleteController {
+    
+    @DeleteMapping(value = "/request1")
+    public String getRequestParam1(@RequestParam String email){
+        return "e-mail : " + email;
+    }
+}
---
+++ springboot_tests/api/src/main/java/com/api/api/controller/PutController.java
@@ -0,0 +1,39 @@
+package com.api.api.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.api.dto.MemberDto;
+
+
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+    
+    @PutMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> putData){
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + "  : "+ map.getValue()+ "\n");
+        });
+        return sb.toString();
+    }
+
+
+    @PutMapping(value="/member1")
+    public String postMemberDto1(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+
+    @PutMapping(value="/member2") //MemberDto 객체를 반환값으로 가지기 때문에 
+                                // 해당 객체안에 오버라이딩된 toString() 함수로 반환된다.
+    public MemberDto postMemberDto2(@RequestBody MemberDto memberDto) {
+        return memberDto;
+    }
+}
